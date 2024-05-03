2b53f2e1a59521c485baf8604ac4d5a7d48579d3
Alex Mills <alex@alexmills.uk>
2024-05-03 22:36:47 +0100

    temporarily restore back to root

--- organization.tf
+++ organization.tf
@@ -27,15 +27,15 @@ resource "aws_organizations_organizational_unit" "workloads" {
 
 resource "aws_organizations_organizational_unit" "workloads_dev" {
   name      = "Development"
-  parent_id = aws_organizations_organizational_unit.workloads.id
+  parent_id = data.aws_organizations_organization.org.roots[0].id
 }
 
 resource "aws_organizations_organizational_unit" "workloads_staging" {
   name      = "Staging"
-  parent_id = aws_organizations_organizational_unit.workloads.id
+  parent_id = data.aws_organizations_organization.org.roots[0].id
 }
 
 resource "aws_organizations_organizational_unit" "workloads_production" {
   name      = "Production"
-  parent_id = aws_organizations_organizational_unit.workloads.id
+  parent_id = data.aws_organizations_organization.org.roots[0].id
 }
