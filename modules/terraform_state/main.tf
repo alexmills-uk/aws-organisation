f9ee8dfe2a50945a273dac54798170a83e7bfea7
Alex Mills <alex@alexmills.uk>
2024-05-03 22:36:10 +0100

    module for terraform state

---
+++ modules/terraform_state/main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0.0"
+    }
+  }
+}
+
+data "aws_region" "current" {}
+data "aws_caller_identity" "current" {}
+
+locals {
+  region                 = data.aws_region.current.name
+  account_id             = data.aws_caller_identity.current.account_id
+}
+
+resource "aws_s3_bucket" "this" {
+  bucket = "tf-state-${local.region}-${local.account_id}"
+}
+
+resource "aws_dynamodb_table" "this" {
+  name           = "tf-state-${local.region}-${local.account_id}"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
