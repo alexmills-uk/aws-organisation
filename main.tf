a404243b317b98747911f73d07e1e91bf86e653f
Alex Mills <alex@alexmills.uk>
2024-05-03 22:35:31 +0100

    reorganise

--- main.tf
+++ main.tf
@@ -2,89 +2,6 @@ provider "aws" {
   region  = var.region
 }
 
-data "aws_ssoadmin_instances" "this" {}
-
-resource "aws_identitystore_group" "administrators" {
-  identity_store_id = tolist(data.aws_ssoadmin_instances.this.identity_store_ids)[0]
-  display_name      = "Administrators"
-  description       = "Administrators of the AWS Organisation"
-}
-
-resource "aws_identitystore_user" "alexm" {
-  identity_store_id = tolist(data.aws_ssoadmin_instances.this.identity_store_ids)[0] 
-
-  display_name = "Alex Mills"
-  user_name    = "alexm"
-
-  name {
-    given_name  = "Alex"
-    family_name = "Mills"
-  }
-
-  emails {
-    value = "alex+user@alexmills.uk"
-  }
-}
-
-resource "aws_identitystore_group_membership" "alexm_administrators" {
-  identity_store_id = tolist(data.aws_ssoadmin_instances.this.identity_store_ids)[0]
-  group_id          = aws_identitystore_group.administrators.group_id
-  member_id         = aws_identitystore_user.alexm.user_id
-}
-
-resource "aws_ssoadmin_permission_set" "admin_permissionset" {
-  name         = "admin-permissionset"
-  instance_arn = tolist(data.aws_ssoadmin_instances.this.arns)[0]
-}
-
-resource "aws_ssoadmin_managed_policy_attachment" "administrator_managed_policy_attachment" {
-  instance_arn       = tolist(data.aws_ssoadmin_instances.this.arns)[0]
-  managed_policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
-  permission_set_arn = aws_ssoadmin_permission_set.admin_permissionset.arn
-}
-
-data "aws_organizations_organization" "org" {}
-
-resource "aws_organizations_organizational_unit" "sandbox" {
-  name      = "Sandbox"
-  parent_id = data.aws_organizations_organization.org.roots[0].id
-}
-
-resource "aws_organizations_organizational_unit" "workloads" {
-  name      = "Workloads"
-  parent_id = data.aws_organizations_organization.org.roots[0].id
-}
-
-resource "aws_organizations_organizational_unit" "suspended" {
-  name      = "Suspended"
-  parent_id = data.aws_organizations_organization.org.roots[0].id
-}
-
-resource "aws_organizations_organizational_unit" "security" {
-  name      = "Security"
- parent_id = data.aws_organizations_organization.org.roots[0].id
-}
-
-resource "aws_organizations_organizational_unit" "exceptions" {
-  name      = "Exceptions"
-  parent_id = data.aws_organizations_organization.org.roots[0].id
-}
-
-resource "aws_organizations_organizational_unit" "workloads_dev" {
-  name      = "Development"
-  parent_id = aws_organizations_organizational_unit.workloads.id
-}
-
-resource "aws_organizations_organizational_unit" "workloads_staging" {
-  name      = "Staging"
-  parent_id = aws_organizations_organizational_unit.workloads.id
-}
-
-resource "aws_organizations_organizational_unit" "workloads_production" {
-  name      = "Production"
-  parent_id = aws_organizations_organizational_unit.workloads.id
-}
-
 resource "aws_organizations_account" "production" {
   name                       = "Production"
   email                      = "alex+production@alexmills.uk"
@@ -99,6 +16,11 @@ resource "aws_organizations_account" "production" {
   parent_id = aws_organizations_organizational_unit.workloads_production.id
 }
 
+resource "aws_identitystore_group_membership" "admin_production" {
+  identity_store_id = tolist(data.aws_ssoadmin_instances.this.identity_store_ids)[0]
+  group_id          = aws_identitystore_group.administrators.group_id
+  member_id         = aws_organizations_account.production.id
+}
 
 provider "aws" {
   alias  = "production"
@@ -121,6 +43,10 @@ provider "aws" {
   ]
 }
 
-resource "aws_s3_bucket" "production_tfstate" {
-  bucket = "tf-state-eu-west-2-${aws_organizations_account.production.id}"
+module "production_terraform_state" {
+  source = "./modules/terraform_state"
+  providers = {
+    aws = aws.production
+  }
+  depends_on = [ aws_organizations_account.production ]
 }
---
+++ organization.tf
@@ -0,0 +1,41 @@
+data "aws_organizations_organization" "org" {}
+
+resource "aws_organizations_organizational_unit" "sandbox" {
+  name      = "Sandbox"
+  parent_id = data.aws_organizations_organization.org.roots[0].id
+}
+
+resource "aws_organizations_organizational_unit" "suspended" {
+  name      = "Suspended"
+  parent_id = data.aws_organizations_organization.org.roots[0].id
+}
+
+resource "aws_organizations_organizational_unit" "security" {
+  name      = "Security"
+ parent_id = data.aws_organizations_organization.org.roots[0].id
+}
+
+resource "aws_organizations_organizational_unit" "exceptions" {
+  name      = "Exceptions"
+  parent_id = data.aws_organizations_organization.org.roots[0].id
+}
+
+resource "aws_organizations_organizational_unit" "workloads" {
+  name      = "Workloads"
+  parent_id = data.aws_organizations_organization.org.roots[0].id
+}
+
+resource "aws_organizations_organizational_unit" "workloads_dev" {
+  name      = "Development"
+  parent_id = aws_organizations_organizational_unit.workloads.id
+}
+
+resource "aws_organizations_organizational_unit" "workloads_staging" {
+  name      = "Staging"
+  parent_id = aws_organizations_organizational_unit.workloads.id
+}
+
+resource "aws_organizations_organizational_unit" "workloads_production" {
+  name      = "Production"
+  parent_id = aws_organizations_organizational_unit.workloads.id
+}
