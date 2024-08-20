fdbee69c41d353325f9861e8417f13c17f61f0fe
Alex Mills <alex@alexmills.uk>
2024-08-20 09:56:01 +0000

    add oidc to the accounts

--- modules/terraform_state/main.tf
+++ modules/terraform_state/main.tf
@@ -29,3 +29,43 @@ resource "aws_dynamodb_table" "this" {
     type = "S"
   }
 }
+
+resource "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com",
+  ]
+
+  thumbprint_list = [
+    "6938fd4d98bab03faadb97b34396831e3780aea1",
+  ]
+}
+
+data "aws_iam_policy_document" "github_oidc_assume_role" {
+  statement {
+    effect = "Allow"
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    principals {
+      type = "Federated"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/token.actions.githubusercontent.com"]
+    }
+
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = ["repo:alexmills-uk/*"]
+    }
+  }
+}
+
+resource "aws_iam_role" "github_oidc_role" {
+  name               = "github-deployment"
+  assume_role_policy = data.aws_iam_policy_document.github_oidc_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "github_oidc_role_policy" {
+  role       = aws_iam_role.github_oidc_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
