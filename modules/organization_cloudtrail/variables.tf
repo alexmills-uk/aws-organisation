c19e1f8d513b33f5a878ea95d66b574529cddceb
Alex Mills <alex@alexmills.uk>
2025-02-10 20:20:50 +0000

    removed audit_account_id

--- modules/organization_cloudtrail/variables.tf
+++ modules/organization_cloudtrail/variables.tf
@@ -12,7 +12,3 @@ variable "log_expiry_days" {
   default     = 365
   description = "The number of days before Cloudtrail logs are deleted"
 }
-
-variable "audit_account_id" {
-  description = "The AWS Account Id for the Audit account for your Organization"
-}
